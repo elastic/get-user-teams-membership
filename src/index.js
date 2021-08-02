ce97eb8fd9f0b7a3fa24e286585b1df14245e075
Alexander Wert <AlexanderWert@users.noreply.github.com>
2021-08-02 08:19:32 +0200

    Changed team check to include child teams

--- src/index.js
+++ src/index.js
@@ -13,52 +13,16 @@ async function run() {
         const username = core.getInput("username")
         const team = core.getInput("team")
 
-        console.log(`Getting teams for ${username} in org ${organization}. Will check if belongs to ${team}`)
+        console.log(`Will check if ${username} belongs to ${team}`)
+        
+        let data = api.rest.teams.getMembershipForUserInOrg({
+              org: organization,
+              team_slug: team,
+              username: username,
+            });
+        
+        let isTeamMember = data.role && data.state === 'active';
 
-        const query = `query($cursor: String, $org: String!, $userLogins: [String!], $username: String!)  {
-            user(login: $username) {
-                id
-            }
-            organization(login: $org) {
-              teams (first:1, userLogins: $userLogins, after: $cursor) { 
-                  nodes {
-                    name
-                }
-                pageInfo {
-                  hasNextPage
-                  endCursor
-                }        
-              }
-            }
-        }`
-
-        let data
-        let teams = []
-        let cursor = null
-
-        // We need to check if the user exists, because if it doesn't exist then all teams in the org
-        // are returned. If user doesn't exist graphql will throw an exception
-        // Paginate
-        do {
-            data = await api.graphql(query, {
-                "cursor": cursor,
-                "org": organization,
-                "userLogins": [username],
-                "username": username
-            })
-            
-            teams = teams.concat(data.organization.teams.nodes.map((val) => {
-                return val.name
-            }))
-
-            cursor = data.organization.teams.pageInfo.endCursor
-        } while (data.organization.teams.pageInfo.hasNextPage)
-
-        let isTeamMember = teams.some((teamName) => {
-            return team.toLowerCase() === teamName.toLowerCase()
-        })
-
-        core.setOutput("teams", teams)
         core.setOutput("isTeamMember", isTeamMember)
 
         console.log(`${username} is member of ${organization}/${team}: ${isTeamMember}`)
